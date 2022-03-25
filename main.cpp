16aa7a74f01e749608de310b6b96daef8346db3c
stanoira <smartineztanoira@itba.edu.ar>
2022-03-24 22:53:33 -0300

    ._.

--- main.cpp
+++ main.cpp
@@ -10,28 +10,31 @@ int main()
 	const int screenHeight = 450;
 
 	InitWindow(screenWidth, screenHeight, "EDA controler");
+	MQTTClient robot("controller");
+	robot.connect("127.0.0.1", 1883, "user", "vdivEMMN3SQWX2Ez");
 
-	MQTTClient control("controller");
-	control.connect("127.0.0.1", 1883, "user", "vdivEMMN3SQWX2Ez");			// IMPORTANTE!!! PRIMERO HACER LO MONO DE LEER LOS DATOS
-	cout << "conectado?" << endl;											// creo q con lo la linea 22 ahi recibo el dato pero no sé como imprimirlo
-	cout << "0 no, 1 si" << endl;
-	cout << "estado = " << control.isConnected() << endl;
+	const string batteryLevel = "robot1/power/batteryLevel";
+	robot.subscribe(batteryLevel);
 
-	control.subscribe("power");											// capaz mejor hacer una clase q se suscriba y lea las cosas
-	control.subscribe("batteryLevel");
+	vector<MQTTMessage> messages;
 
-	/* hacer función q printee los valores de un vector<MQTTMessage>
-	seguramente hacer una clase q maneje todo esto
-	deberia ser como un printf del topic y otro del payload */
-
+	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("Anda Pibe!",
-			340, 200, 20, LIGHTGRAY);
-		vector<MQTTMessage> mensaje = control.getMessages();
-		cout << mensaje.data()->topic << endl;
+		DrawText("Anda A Laburar!", 340, 200, 20, LIGHTGRAY);
+
+		messages = robot.getMessages();
+		if (messages.size())
+		{
+			for (int i = 0; i < messages.size(); i++)
+			{
+				cout << "MIRA ACA" << endl;
+				cout << messages.data()->topic << endl;
+			}
+		}
+
 		EndDrawing();
 	}
 	CloseWindow();
